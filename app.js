f8f979a4cebe9f73ded463052942fb5a0c43a187
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2022-12-12 11:09:15 +0100

    Add "sauce" controller

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const saucesRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 mongoose
   .connect(
@@ -10,11 +12,48 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+app.use(express.json());
 
-app.use ((req, resp) => {
-    resp.json({
-        message: "Votre requête à bien été reçue ! "
-    })
-})
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// app.post("/api/auth/signup", (req, res, next)=>{
+//     const user =
+// })
+
+// Test middleware sur postman
+app.get("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
 
 module.exports = app;
---
+++ controllers/sauce.js
@@ -0,0 +1,29 @@
+const Sauce = require('../models/sauce');
+
+exports.createSauce = (req, res, next) => {
+  const sauce = new Sauce({
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    usersLikes: req.body.usersLiked,
+    usersDisliked: req.body.usersDisliked
+  });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Sauce saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
