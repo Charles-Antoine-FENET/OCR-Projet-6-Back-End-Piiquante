c14bff68fb804040233a9e11322f8917c80cefff
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2022-12-13 15:45:49 +0100

    Add multer archi and config middleware and routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
+const path = require('path');
 
 mongoose
   .connect(
@@ -29,5 +30,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use("/api/sauces", sauceRoutes);
 app.use("/api/auth", userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
